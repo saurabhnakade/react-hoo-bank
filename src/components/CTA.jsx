8b319f2baf957f77e0031cff753bf3ee006c52db
saurabhnakade <saurabh.nakade23@gmail.com>
2022-08-20 23:24:40 +0530

    cta section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
-    </div>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col
+    bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex-1 flex flex-col'>
+        <h2 className={styles.heading2}>Lets try our service now</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, minus! Quis impedit velit reprehenderit incidunt.
+        </p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10 `}>
+        <Button/>
+      </div>
+    </section>
   )
 }
 
